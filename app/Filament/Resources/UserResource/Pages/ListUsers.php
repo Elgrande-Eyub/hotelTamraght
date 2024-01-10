f9bbc4df65daaed1e24a2e12a1284b69e61a17e1
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2024-01-10 17:01:55 +0100

    update

---
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab ;
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    public function getTabs():array {
+        return[
+            'all' => Tab::make('Toutes '),
+            'archived' => Tab::make('Archivé')->modifyQueryUsing(function($query){
+                return $query->onlyTrashed();
+            })
+        ];
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
